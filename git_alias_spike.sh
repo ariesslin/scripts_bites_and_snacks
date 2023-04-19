c3c2ff04a0112bb886bb73d2599af6e79de856d2
ariesslin <ariess.lin@gmail.com>
2023-04-19 02:16:29 +0200

    Update git_alias_spike.sh

--- git_alias_spike.sh
+++ git_alias_spike.sh
@@ -2,7 +2,7 @@
 
 # Define git alias
 alias_name="c"
-alias_command="\"!f() { read -p \\\"Enter card number: \\\" card_number; read -p \\\"Enter pair: \\\" pair; read -p \\\"Enter commit message: \\\" commit_message; commit_msg=\\\"\$card_number | \$pair | \$commit_message\\\"; git commit -m \\\"\$commit_msg\\\"; }; f\""
+alias_command='!f() { read -p "Enter card number: " card_number; read -p "Enter pair: " pair; read -p "Enter commit message: " commit_message; commit_msg="$card_number | $pair | $commit_message"; git commit -m "$commit_msg"; }; f'
 
 # Add alias to global git configuration file
 git config --global alias.$alias_name "$alias_command"
